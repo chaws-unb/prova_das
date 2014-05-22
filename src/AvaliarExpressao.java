af88ac8d4bd4d2a9f0f54475c23c5fce20e624a5
Charles Oliveira <chaws@localhost.localdomain>
2014-05-22 18:26:38 -0300

    Adicionando validacao da expressao

--- src/AvaliarExpressao.java
+++ src/AvaliarExpressao.java
@@ -4,14 +4,15 @@ import java.util.Stack;
 public class AvaliarExpressao {
 
 	private String entrada; 
-	private Stack<Integer> pilha = new Stack();
+	private Stack<String> pilha = new Stack();
 	
-	public AvaliarExpressao(String entrada) {
+	public AvaliarExpressao(String entrada) throws IllegalArgumentException {
 		this.entrada = entrada;
 		this.separarExpressao();
+		validarExpressao();
 	}
 
-	public int pop() {
+	public String pop() {
 		return pilha.pop();
 	}
 	
@@ -19,38 +20,46 @@ public class AvaliarExpressao {
 	{
 		String[] expressao = this.entrada.split(" ");
 		for (int i = 0; i < expressao.length; i++){
-			char caractere = expressao[i].charAt(0); 
-			pilha.push((int)caractere);
+			pilha.push(expressao[i]);
 		}
 		
 	}
 
 	public String converterPostFix() {
-		Stack<Integer> temporaria = new Stack();
+		Stack<String> temporaria = new Stack();
 		String postfixo = "";
-		Integer charAtual = ' ';
+		String charAtual = "";
 		
 		for(int i = 0; i < pilha.size(); i++){
 			charAtual = pilha.get(i);
-			if('0' <= charAtual && charAtual <= '9'){
-				postfixo += String.valueOf(charAtual) + " ";
-			}
-			else if (charAtual == '('){
+			if (charAtual.equals("(")){
 				temporaria.push(charAtual);
 			}
-			else if (charAtual == ')'){
+			else if( charAtual.equals(")") ){
 				postfixo += temporaria.pop() + " ";
+				temporaria.pop();
+			}
+			else if ("+-/*".contains(charAtual)){
+				temporaria.push(charAtual);
 			}
 			else {
-				temporaria.push(charAtual);
-			}
-			if (pilha.size()-1 == i){
-				postfixo += temporaria.pop() + " ";
+				postfixo += charAtual + " ";
 			}
 			
+			if (pilha.size() - 1 == i){
+				postfixo += temporaria.pop() + " ";
+			}
 		}
 		
 		return postfixo;
+	}
+	
+	private void validarExpressao() throws IllegalArgumentException{
+		for(int i = 0; i < pilha.size(); i++){
+			if(!"0123456789()+-*/".contains(pilha.get(i)))
+				throw new IllegalArgumentException("Apenas numeros, operadores e parenteses");
+		}
+		
 	}
 	
 }
--- src/TestAvaliarExpressao.java
+++ src/TestAvaliarExpressao.java
@@ -6,27 +6,33 @@ import org.junit.Test;
 public class TestAvaliarExpressao {
 
 	@Test
-	public void testArmazenarMaisDeUmToken() {
+	public void testArmazenarMaisDeUmToken() throws IllegalArgumentException {
 		String entrada = "3 + 2 * 1 / 0 ( ) -";
 		AvaliarExpressao expressao1 = new AvaliarExpressao(entrada);
 		
-		assertEquals('-', expressao1.pop());
-		assertEquals(')', expressao1.pop());
-		assertEquals('(', expressao1.pop());
-		assertEquals('0', expressao1.pop());
-		assertEquals('/', expressao1.pop());
-		assertEquals('1', expressao1.pop());
-		assertEquals('*', expressao1.pop());
-		assertEquals('2', expressao1.pop());
-		assertEquals('+', expressao1.pop());
-		assertEquals('3', expressao1.pop());
+		assertEquals("-", expressao1.pop());
+		assertEquals(")", expressao1.pop());
+		assertEquals("(", expressao1.pop());
+		assertEquals("0", expressao1.pop());
+		assertEquals("/", expressao1.pop());
+		assertEquals("1", expressao1.pop());
+		assertEquals("*", expressao1.pop());
+		assertEquals("2", expressao1.pop());
+		assertEquals("+", expressao1.pop());
+		assertEquals("3", expressao1.pop());
 	}
 	
 	@Test
-	public void testConversaoPostfix(){
+	public void testConversaoPostfix() throws IllegalArgumentException{
 		String infixo = "( 2 + 2 ) / ( 1 + 1 )";
 		AvaliarExpressao expressao1 = new AvaliarExpressao(infixo);
-		assertEquals("2 2 + 1 1 + /", expressao1.converterPostFix());
+		assertEquals("2 2 + 1 1 + / ", expressao1.converterPostFix());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testValidarExpressao() throws IllegalArgumentException{
+		String infixo = "( 2 + 2 ) / ( 1 + 1 ) l";
+		AvaliarExpressao expressao1 = new AvaliarExpressao(infixo);
 	}
 
 }
